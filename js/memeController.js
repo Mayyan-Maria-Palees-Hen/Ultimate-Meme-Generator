b77d4b1d29b9eab461f3a0bf589a49b05ef86e71
Mayyan Maria <mariapalees@gmail.com>
2024-12-22 17:46:55 +0200

    add memeservice

--- js/memeController.js
+++ js/memeController.js
@@ -17,6 +17,12 @@ function renderMeme() {
     img.src = `img/${meme.selectedImgId}.jpg`
     img.onload = () => {
         gCtx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight)
+        const line = meme.lines[meme.selectedLineIdx]
+        gCtx.font = `${line.size}px Ariel`
+        gCtx.fillStyle = line.color
+        gCtx.fillText(line.txt, 50, 50)
+
     }
+
 }
 
